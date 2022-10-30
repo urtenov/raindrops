68ee367a30d4ec88c160340387fab4232ce62a38
urtenov <82385687+urtenov@users.noreply.github.com>
2022-10-30 15:59:20 +0300

    Update raindrops.py

--- raindrops.py
+++ raindrops.py
@@ -5,4 +5,14 @@ def convert(number):
 'Plong', если число number делится на 7,
 само число number, если оно не делится ни на 3, ни на 5, ни на 7."""
     # Напишите ваш код здесь
-    return str(number) # эту строку можно удалить
+    s=''
+    if number%3==0:
+        s+='Pling'
+    if number%5==0:
+        s+='Plang'
+    if number%7==0:
+        s+='Plong'
+    if s=='':
+        a=str(number)
+        s+=a
+    return s    
